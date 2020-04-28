7e1a74fa3480c8edc909f65354115c6e495a504d
Huy Tran <tranquanghuy999@gmail.com>
2020-04-27 22:42:18 -0700

    creating menu and credit scene

---
+++ Assets/Scripts/Scene Changes/MenuButtons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
