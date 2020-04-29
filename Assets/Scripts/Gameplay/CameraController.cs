f6d294d51abae823b94a1a747263ccd401ea8256
Huy Tran <tranquanghuy999@gmail.com>
2020-04-28 22:30:52 -0700

    add gem prefabs, rotation

--- Assets/Scripts/Gameplay/CameraController.cs
+++ Assets/Scripts/Gameplay/CameraController.cs
@@ -16,4 +16,5 @@ public class CameraController : MonoBehaviour
     {
         this.transform.position = player.transform.position + offset;
     }
+
 }
---
+++ Assets/Scripts/Gameplay/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float speed;
+
+    private Vector3 rotation;
+    
+    void Start()
+    {
+        rotation = new Vector3(45, 15, 90);
+    }
+
+    void Update()
+    {
+        this.transform.Rotate(rotation * speed * Time.deltaTime);
+    }
+}
