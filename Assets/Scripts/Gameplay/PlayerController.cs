92b5bc8a9fed8868b7ae74c4454696ad920ef636
Huy Tran <tranquanghuy999@gmail.com>
2020-04-28 23:01:49 -0700

    make gems consumable

--- Assets/Scripts/Gameplay/PlayerController.cs
+++ Assets/Scripts/Gameplay/PlayerController.cs
@@ -8,9 +8,12 @@ public class PlayerController : MonoBehaviour
     private Rigidbody player;
     public float speed;
 
+    private int score;
+
     void Start()
     {
-        player = GetComponent<Rigidbody>(); 
+        player = GetComponent<Rigidbody>();
+        score = 0;
     }
 
     void FixedUpdate()
@@ -20,7 +23,16 @@ public class PlayerController : MonoBehaviour
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         player.AddForce(movement * speed * Time.deltaTime);
+    }
 
-
+    void OnTriggerEnter(Collider collider)
+    {
+        // check if the collider is of tag 'gem'
+        if (collider.gameObject.CompareTag("Gem"))
+        {
+            collider.gameObject.SetActive(false);
+            score++;
+            Debug.Log("Gem count: " + score);
+        }
     }
 }
