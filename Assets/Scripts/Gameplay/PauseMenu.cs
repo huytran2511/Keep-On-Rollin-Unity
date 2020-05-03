37cf1e9aeb3a7d08773a30c0e8b7c02dcc2c287a
Huy Tran <tranquanghuy999@gmail.com>
2020-05-02 21:41:46 -0700

    add background music

---
+++ Assets/Scripts/Gameplay/PauseMenu.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gamePaused = false;
+    private TMP_Text highscore_PMText;
+    public GameObject pauseMenuUI;
+
+    void Awake()
+    {
+        highscore_PMText = GameObject.Find("HighScore_PM").GetComponent<TMP_Text>();
+        pauseMenuUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        //if (!PlatformCollector.gameOver && !Player.finish)
+        //{
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (gamePaused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
+            }
+        //}
+        //highscore_PMText.text = "High Score: " + Player.highscore.ToString();
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+    public void Restart()
+    {
+        gamePaused = false;
+        Time.timeScale = 1f;
+        //Player.score = 0;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GoHome()
+    {
+        gamePaused = false;
+        Time.timeScale = 1f;
+        //Player.score = 0;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
